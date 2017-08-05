34445bf63f49c87399e9f113c52c40f0d6c2c0a2
SeladangHijau <nadzmiidzham@gmail.com>
2017-08-05 10:27:15 +0800

    Added new branch for result page and front page. Start designing result page.

--- app/src/main/java/com/g_crime/Fragment/Result.java
+++ app/src/main/java/com/g_crime/Fragment/Result.java
@@ -1,6 +1,37 @@
 package com.g_crime.Fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.g_crime.R;
 
 public class Result extends Fragment {
+    // views
+
+    // vars
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_result, container, false);
+
+        initViews();
+        initListener();
+
+        return v;
+    }
+
+    // initialization ------------------------------------------------------------------------------
+    private void initViews() {
+
+    }
+
+    private void initListener() {
+
+    }
+    // ---------------------------------------------------------------------------------------------
 }
