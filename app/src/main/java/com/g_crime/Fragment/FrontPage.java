aa4adb5a3cefbe73aca81b260b61a85e9348cece
officialzaim <officialzaim@gmail.com>
2017-08-06 14:58:21 +0800

    FrontPage no ActionBar

--- app/src/main/java/com/g_crime/Fragment/FrontPage.java
+++ app/src/main/java/com/g_crime/Fragment/FrontPage.java
@@ -3,6 +3,7 @@ package com.g_crime.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,9 @@ public class FrontPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_front_page, container, false);
         Button btnStart = (Button) v.findViewById(R.id.btnStart);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
